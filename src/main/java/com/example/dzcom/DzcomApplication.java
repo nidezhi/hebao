4b8cedb4bfc1bd10b8ffb5dd5332af59082e1362
hebao <hebao@yanhong.com>
2026-05-30 15:47:52 +0800

    md 初始化

--- src/main/java/com/example/dzcom/DzcomApplication.java
+++ src/main/java/com/example/dzcom/DzcomApplication.java
@@ -1,9 +1,11 @@
 package com.example.dzcom;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.dzcom.infrastructure.dao.mapper")
 public class DzcomApplication {
 
     public static void main(String[] args) {
